4813ff75211c61941b9ccc435ac2a82ee2ffba98
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-03-24 07:49:29 -0300

    Integrated Composition API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-	buildModules: ['@nuxt/typescript-build'],
+	buildModules: ['@nuxt/typescript-build', '@nuxtjs/composition-api'],
 	components: true,
+	generate: {
+		interval: 2000,
+	},
 	srcDir: 'src',
 	target: 'static',
 }
